ae8a60e447d812880780e4595b9b80a47c73aec6
David-Lucas17 <lucasehermilda@gmail.com>
2025-09-02 07:19:13 -0300

    refactor: correction

--- workflow.js
+++ workflow.js
@@ -139,6 +139,12 @@ define(['jquery', 'fab/fabrik', 'lib/debounce/jquery.ba-throttle-debounce'], fun
 							fields[key].parentElement.setAttribute('title', "Completar ou corrigir esses dados")
 						};
 					});
+
+					if(!self.options.user.hasPermission) {
+						var link = jQuery(btnGroup[0]).find('.fabrik_edit');
+						var span = link.find('span').first();
+						link.html('<span>' + span.html() + '</span> ' + Joomla.JText._("PLG_FORM_WORKFLOW_REPORT_EDIT_RECORD_LIST"));
+					}
 				});
 
 				jQuery("a.btn-default-delete").on("click", debounce(2500, true, function (e) {
@@ -1101,7 +1107,7 @@ define(['jquery', 'fab/fabrik', 'lib/debounce/jquery.ba-throttle-debounce'], fun
 						easyadmin.setElementLabelAdvancedLink('_wfl');
 						easyadmin.showHideElements('show_in_list', 'element', 'yesno', '', '_wfl');
 
-						jQuery('#easyadmin_modal___type_wfl').trigger('change');
+						jQuery('#easyadmin_modal___type_wfl').trigger('change', {sufix: '_wfl'});
 						jQuery('label[for="easyadmin_modal___label_advanced_link_wfl"]').trigger('click', {button: 'edit-element', sufix: '_wfl'});
 						jQuery('#easyadmin_modal___options_dropdown_wfl').attr('disabled', true);
 						jQuery('.modalContainer #jlow_fabrik_easyadmin_modal___list-auto-complete').attr('disabled', true);
